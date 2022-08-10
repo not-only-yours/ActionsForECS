66a14f9c7945fa591a4c7f5f8aeb4478b36f567d
techops-root <66493344+techops-root@users.noreply.github.com>
2022-08-10 13:45:25 +0300

    add cert

--- Terraform/FrontendALB.tf
+++ Terraform/FrontendALB.tf
@@ -19,3 +19,16 @@ resource "aws_lb_listener" "frontend-alb_80" {
     target_group_arn = module.fargate-frontend.target_group_arn[0]
   }
 }
+    
+resource "aws_lb_listener" "frontend-alb_443" {
+  load_balancer_arn = module.frontend-alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:eu-west-2:881750644134:certificate/a753ec86-8554-4bb4-a099-6b027d305980"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = module.fargate-frontend.target_group_arn[0]
+  }
+}    
