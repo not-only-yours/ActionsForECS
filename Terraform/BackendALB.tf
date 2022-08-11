b02bb5e418c435fa3f814da54feee31a7b1dca09
not-only-yours <darty.vis@gmail.com>
2022-08-11 15:59:33 +0300

    update configuration internal lb

--- Terraform/BackendALB.tf
+++ Terraform/BackendALB.tf
@@ -1,16 +1,31 @@
-module "backend-alb" {
-  source  = "umotif-public/alb/aws"
-  version = "~> 2.0"
-
-  name_prefix        = "alb-backend"
+resource "aws_lb" "backend" {
+  name               = "test-lb-tf"
+  internal           = true
   load_balancer_type = "application"
-  internal           = false
-  vpc_id             = module.vpc.vpc_id
+  security_groups    = [aws_security_group.backend_lb]
   subnets            = module.vpc.private_subnets
+
+
+  tags = {
+    Environment = "production"
+  }
 }
 
+
+
+#module "backend-alb" {
+#  source  = "umotif-public/alb/aws"
+#  version = "~> 2.0"
+#
+#  name_prefix        = "alb-backend"
+#  load_balancer_type = "application"
+#  internal           = false
+#  vpc_id             = module.vpc.vpc_id
+#  subnets            = module.vpc.private_subnets
+#}
+
 resource "aws_lb_listener" "backend-alb-3000" {
-  load_balancer_arn = module.backend-alb.arn
+  load_balancer_arn = aws_lb.backend
   port              = "3000"
   protocol          = "HTTP"
 
--- Terraform/BackendSecurityGroup.tf
+++ Terraform/BackendSecurityGroup.tf
@@ -1,21 +1,29 @@
 #####
 # Security Group Config
 #####
-resource "aws_security_group_rule" "alb_ingress_3000" {
-  security_group_id = module.backend-alb.security_group_id
-  type              = "ingress"
-  protocol          = "tcp"
-  from_port         = 3000
-  to_port           = 3000
-  cidr_blocks       = var.ALL_CIDR_BLOCKS
-  #ipv6_cidr_blocks  = ["::/0"]
-}
+resource "aws_security_group" "backend_lb" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
 
-resource "aws_security_group_rule" "task_ingress_3000" {
-  security_group_id        = module.fargate-backend.service_sg_id
-  type                     = "ingress"
-  protocol                 = "tcp"
-  from_port                = 3000
-  to_port                  = 3000
-  source_security_group_id = module.backend-alb.security_group_id
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = var.ALL_CIDR_BLOCKS
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "internal"
+  }
 }
