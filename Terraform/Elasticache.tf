a032682dde84a1f21f1aaf817ead1cf4ae5276a2
techops-root <66493344+techops-root@users.noreply.github.com>
2022-08-21 14:53:39 +0300

    edit

--- Terraform/Elasticache.tf
+++ Terraform/Elasticache.tf
@@ -1,38 +1,29 @@
-#resource "aws_security_group" "redis" {
-#  vpc_id = module.vpc.vpc_id
-#  ingress {
-#    from_port = var.ELASTICCACHE_PORT
-#    to_port = var.ELASTICCACHE_PORT
-#    protocol = "tcp"
-#    cidr_blocks = var.ALL_CIDR_BLOCKS
-#  }
-#
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#   protocol    = "-1"
-#    cidr_blocks = var.ALL_CIDR_BLOCKS
-#  }
-#}
-#
-#
-#resource "aws_elasticache_cluster" "enabled" {
-#
-#  cluster_id           = "aws-ecs-cluster"
-#  engine               = "redis"
-#  node_type            = "cache.t2.micro"
-#  num_cache_nodes      = 1
-#  parameter_group_name = "default.redis6.x"
-#  port                 = var.ELASTICCACHE_PORT
-#  security_group_ids   = [aws_security_group.redis.id]
-#  subnet_group_name    = module.vpc.elasticache_subnet_group_name
-#}
-#
-##resource "aws_elasticache_user" "test" {
-##  user_id       = "testUserId"
-##  user_name     = "testUserName"
-##  access_string = "on ~app::* -@all +@read +@hash +@bitmap +@geo -setbit -bitfield -hset -hsetnx -hmset -hincrby -hincrbyfloat -hdel -bitop -geoadd -georadius -georadiusbymember"
-##  engine        = "REDIS"
-##  passwords     = ["password123456789"]
-##}
-##
+resource "aws_security_group" "redis" {
+  vpc_id = module.vpc.vpc_id
+  ingress {
+    from_port = var.ELASTICCACHE_PORT
+    to_port = var.ELASTICCACHE_PORT
+    protocol = "tcp"
+    cidr_blocks = var.ALL_CIDR_BLOCKS
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+   protocol    = "-1"
+    cidr_blocks = var.ALL_CIDR_BLOCKS
+  }
+}
+
+  
+resource "aws_elasticache_cluster" "enabled" {
+
+  cluster_id           = "aws-ecs-cluster"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis6.x"
+  port                 = var.ELASTICCACHE_PORT
+  security_group_ids   = [aws_security_group.redis.id]
+  subnet_group_name    = module.vpc.elasticache_subnet_group_name
+}
