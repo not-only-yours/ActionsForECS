e696befe4532b476837185dd3fbb96fd13adf12e
techops-root <66493344+techops-root@users.noreply.github.com>
2022-08-30 14:56:42 +0300

    Update and rename .route53.tf. to Route53.tf

---
+++ Terraform/Route53.tf
@@ -0,0 +1,18 @@
+#Create a new Hosted Zone 
+
+resource "aws_route53_zone" "test" {
+  name = var.DNS
+}
+
+#Standard route53 DNS record for "test" pointing to an ALB
+
+resource "aws_route53_record" "test" {
+  zone_id = aws_route53_zone.test.zone_id
+  name    = aws_route53_zone.test.name
+  type    = "A"
+  alias {
+    name                   = "${module.frontend-alb.dns_name}"
+    zone_id                = "${module.frontend-alb.zone_id}"
+    evaluate_target_health = false
+  }
+}
